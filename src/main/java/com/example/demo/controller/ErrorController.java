0ae0bd16ed06287801bae978b3f2512ff51e709a
1019864511 <51944359+1019864511@users.noreply.github.com>
2020-04-21 15:46:20 +0800

    2020.4.21 update by  XingLiu 增加管理员显示页面

---
+++ src/main/java/com/example/demo/controller/ErrorController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description:错误页面拦截器
+ * @Author: Xingliu
+ * @Verion: 1.0
+ * @TIME:Created in 8:13 2020/4/21
+ */
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+    @GetMapping(value = "/404")
+    public String error_400(){
+        return "comm/error_404";
+    }
+
+
+    @GetMapping(value = "/500")
+    public String error_500(){
+        return "comm/error_500";
+    }
+}
