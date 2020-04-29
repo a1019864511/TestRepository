470aa4c2468484e143bd865cf908f150109e0090
1019864511 <51944359+1019864511@users.noreply.github.com>
2020-04-29 16:40:48 +0800

    2020.4.28 update by  XingLiu 整合Swagger2.0实现在线api文档

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.Services.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class AdminController {
-
+    @Autowired
+    private RedisService redisService;
     @RequestMapping("/adminindex")
     public String getAdminIndex(){
+        redisService.set("11","从缓存获取数据");
+        System.out.println(redisService.get("11"));
         return "/Admin/AdminIndex";
     }
 
